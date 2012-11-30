729b95ecaa25d153f1a0cb79d772e63a4ef6677f
Alexey Kiryukhin <akiryuhin@mirantis.com>
2012-11-30 16:44:33 +0400

    Thucydides version 0.9.77

--- jbehave-thucydides-sample-project/src/test/java/net/thucydides/sample/AcceptanceTest.java
+++ jbehave-thucydides-sample-project/src/test/java/net/thucydides/sample/AcceptanceTest.java
@@ -5,6 +5,12 @@ import net.thucydides.jbehave.ThucydidesJUnitStories;
 
 public class AcceptanceTest extends ThucydidesJUnitStories {
 	public AcceptanceTest() {
+		
+//		Parallezizing using JBehave threads - it works, but Thucydides has problems with screenshoting in this case
+//		configuredEmbedder().embedderControls().doGenerateViewAfterStories(true).doIgnoreFailureInStories(true)
+//        .doIgnoreFailureInView(true).doVerboseFiltering(true).useThreads(2).useStoryTimeoutInSecs(2);
+
+//		How to run a single story
 //		findStoriesCalled("**/topics/*.story");
 	}
 }
